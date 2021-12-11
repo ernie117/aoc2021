fc7c13593f5773908829649ebf89963a3fd87606
ernie117 <ern3st@hotmail.co.uk>
2021-12-11 16:22:07 +0000

    Adding input files. Adding first day solution.

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,5 +1,30 @@
 module Day1 where
 
+getInput :: IO [Int]
+getInput = fmap (map read . lines) (readFile "input/input-day1.txt")
+
+countIncreases :: [Int] -> Int
+countIncreases [] = 0
+countIncreases [_] = 0
+countIncreases (x:y:zs)
+  | y > x = 1 + countIncreases (y : zs)
+  | otherwise = countIncreases (y : zs)
+
+triple :: [Int] -> [Int]
+triple [] = []
+triple [x] = []
+triple [x, y] = []
+triple (x:y:z:rest) = x + y + z : triple (y : z : rest)
+
+solution1 :: [Int] -> Int
+solution1 = countIncreases
+
+solution2 :: [Int] -> Int
+solution2 = countIncreases . triple
+
+-- Part 1 answer: 1722
+-- Part 2 answer: 1748
 main :: IO ()
 main = do
-  print "something"
+  getInput >>= print . countIncreases
+  getInput >>= print . solution2
