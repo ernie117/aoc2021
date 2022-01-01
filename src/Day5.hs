356d27f690a829fbb5ccb62ab2c4589b22d0d595
ernie117 <ern3st@hotmail.co.uk>
2022-01-01 21:56:04 +0000

    qualified imports

--- src/Day5.hs
+++ src/Day5.hs
@@ -3,21 +3,21 @@ module Day5 where
 import Data.Char
 import Data.Foldable (toList)
 import Data.Function
-import Data.List hiding (intersperse)
+import Data.List
 import Data.List.Split
-import Data.Sequence (Seq, (><), filter, fromList, index, intersperse, update)
+import qualified Data.Sequence as SQ
 
 type Dimensions = (Int, Int)
 
 type Range = [Dimensions]
 
-type Sequences = Seq (Seq Char)
+type Sequences = SQ.Seq (SQ.Seq Char)
 
 data Matrix =
   Matrix Sequences Range
 
 instance Show Matrix where
-  show (Matrix sq _) = toList $ foldr1 (><) (intersperse (fromList "\n") sq)
+  show (Matrix sq _) = toList $ foldr1 (SQ.><) (SQ.intersperse (SQ.fromList "\n") sq)
 
 pair :: String -> (Int, Int)
 pair = go . splitOn ","
@@ -28,7 +28,7 @@ getInput :: IO [String]
 getInput = fmap lines (readFile "input/input-day5.txt")
 
 getCoords :: [String] -> Range
-getCoords = concatMap (expand . map pair . Prelude.filter (/= "->") . words)
+getCoords = concatMap (expand . map pair . filter (/= "->") . words)
 
 expand :: Range -> Range
 expand r
@@ -52,14 +52,14 @@ expand r
     fillDescY                    = [(x1, y) | y <- [y1,(pred y1) .. y2]]
 
 incLine :: Dimensions -> Sequences -> Sequences
-incLine (x, y) sqs = update x (increment y line) sqs
+incLine (x, y) sqs = SQ.update x (increment y line) sqs
   where
-    line = index sqs x
+    line = SQ.index sqs x
     increment idx sqss
-      | element == '.' = update idx '1' sqss
-      | otherwise = update idx (intToDigit (succ (digitToInt element))) sqss
+      | element == '.' = SQ.update idx '1' sqss
+      | otherwise = SQ.update idx (intToDigit (succ (digitToInt element))) sqss
       where
-        element = index sqss idx
+        element = SQ.index sqss idx
 
 updateMatrix :: Matrix -> Matrix
 updateMatrix (Matrix sqs (r:rs)) = Matrix (incLine r sqs) rs
@@ -73,7 +73,7 @@ dimensions rs = (maxX rs, maxY rs)
 buildMatrix :: Range -> Matrix
 buildMatrix rs =
   Matrix
-    (fromList (replicate (snd ds + 1) $ fromList (replicate (fst ds + 1) '.')))
+    (SQ.fromList (replicate (snd ds + 1) $ SQ.fromList (replicate (fst ds + 1) '.')))
     rs
   where
     ds = dimensions rs
@@ -83,7 +83,7 @@ rangeEmpty (Matrix _ rs) = null rs
 
 countOverlaps :: Matrix -> Int
 countOverlaps (Matrix sqs _) =
-  length $ Data.Sequence.filter (>= '2') $ foldr1 (><) sqs
+  length $ SQ.filter (>= '2') $ foldr1 (SQ.><) sqs
 
 solve1 :: Matrix -> Int
 solve1 = countOverlaps . until rangeEmpty updateMatrix
