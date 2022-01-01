826c1471fb0490c22bc085c6f5afc76a2cb63a01
ernie117 <ern3st@hotmail.co.uk>
2022-01-01 23:34:39 +0000

    more tidying up

--- src/Day5.hs
+++ src/Day5.hs
@@ -7,37 +7,28 @@ type Dimensions = (Int, Int)
 
 type Range = [Dimensions]
 
-pair :: String -> (Int, Int)
-pair = go . splitOn ","
-  where
-    go [x, y] = (read x, read y)
-
 getInput :: IO [String]
 getInput = fmap lines (readFile "input/input-day5.txt")
 
 getCoords :: [String] -> Range
-getCoords = concatMap (expand . map pair . filter (/= "->") . words)
+getCoords =
+  concatMap expand . straights . map (map pair . filter (/= "->") . words)
+
+pair :: String -> Dimensions
+pair = go . splitOn ","
+  where
+    go [x, y] = (read x, read y)
+
+straights :: [Range] -> [Range]
+straights = filter (\[(x1, y1), (x2, y2)] -> x1 == x2 || y1 == y2)
 
 expand :: Range -> Range
-expand r
-  | isDiagonal  = []
-  | ascendingX  = fillAscX
-  | descendingX = fillDescX
-  | ascendingY  = fillAscY
-  | descendingY = fillDescY
-  | otherwise   = r
-    where
-      unpack ((x1, y1):(x2, y2):_) = (x1, y1, x2, y2)
-      (x1, y1, x2, y2)             = unpack r
-      isDiagonal                   = x1 /= x2 && y1 /= y2
-      ascendingX                   = x1 < x2
-      descendingX                  = x1 > x2
-      ascendingY                   = y1 < y2
-      descendingY                  = y1 > y2
-      fillAscX                     = zip [x1 .. x2] (repeat y1)
-      fillDescX                    = zip [x1,(pred x1) .. x2] (repeat y1)
-      fillAscY                     = zip (repeat x1) [y1 .. y2]
-      fillDescY                    = zip (repeat x1) [y1,(pred y1) .. y2]
+expand ((x1, y1):(x2, y2):_) = zip (compare' x1 x2) (compare' y1 y2)
+  where
+    compare' w z
+      | w < z = [w .. z]
+      | w > z = [w,(pred w) .. z]
+      | otherwise = repeat w
 
 solve1 :: Range -> Int
 solve1 = length . filter (>= 2) . map length . group . sort
