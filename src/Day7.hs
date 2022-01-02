c750e6888e285540b99658e5c5fd82dbe6d1d348
ernie117 <ern3st@hotmail.co.uk>
2022-01-02 17:54:00 +0000

    formatting

--- src/Day7.hs
+++ src/Day7.hs
@@ -8,13 +8,13 @@ getInput :: IO [Int]
 getInput = map read . splitOn "," <$> readFile "input/input-day7.txt"
 
 mapNumbers :: [Int] -> Int
-mapNumbers xs = snd $ minimumBy (compare `on` snd) $ (map doSubtract xs)
+mapNumbers xs = snd $ minimumBy (compare `on` snd) $ map doSubtract xs
   where
     doSubtract i = (i, sum $ map (\x -> abs (i - x)) xs)
 
 -- Very slow; found this faster calculation: (diff^2 + abs diff) `div` 2
 mapNumbers2 :: [Int] -> Int
-mapNumbers2 xs = snd $ minimumBy (compare `on` snd) $ (map doSubtract xs)
+mapNumbers2 xs = snd $ minimumBy (compare `on` snd) $ map doSubtract xs
   where
     doSubtract i = (i, sum $ map (\x -> sum [1..(abs (i - x))]) xs)
 
